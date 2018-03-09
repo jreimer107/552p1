e0884f242f570042861d2f6df63b407e7ebcfda1
John Reimer <jreimer107@gmail.com>
2018-03-09 15:34:17 -0600

    Added Shifter

--- ALU.v
+++ ALU.v
@@ -6,11 +6,31 @@ module ALU(A, B, op, out, zero);
 
   wire [15:0] out_and, out_or, out_op, B_op;
   wire cin, cout;
+  wire invert;
 
   localparam  op_or = 2'b00;
   localparam  op_and = 2'b01;
   localparam  op_op = 2'b1x;
 
+  /* ALUOP
+  * The op signal sent to the alu determines what it does.
+  * TODO: edit these as necessary.
+  * Memory instructions (opcode 10xx) should use ADD.
+  * Control instructions (opcode 11xx) should use SUBTRACT.
+  * Arithmetic instructions (opcode 0xxx) use whatever is necessary.
+  * ADD (0000) uses ADD.
+  * SUB (0001) uses SUBTRACT.
+  * RED (0010) uses ?
+  * XOR (0011) uses XOR.
+  * SLL (0100) uses SHIFT LOGICAL.
+  * SRA (0101) uses SHIFT ARITHMETIC.
+  * ROR (0110) uses ROTATE.
+  * PADDSB (0111) uses ?
+  */
+
+  //TODO: I think we're going to have to spread this out; we need to attach
+  //  a CSA tree in here and attach muxes for reduction and saturation. I
+  // think that the reduction problem from the hell exam was a hint.
   CLA_16bit adder(.A(A), .B(B_op), .cin(cin), .cout(cout), .S(out_op));
 
   assign out_or = A | B;
---
+++ Shifter.v
@@ -0,0 +1,47 @@
+module Shifter (Shift_Out, Shift_In, Shift_Val, Mode);
+  input [15:0] Shift_In; //This is the number to perform shift operation on
+  input [3:0] Shift_Val; //Shift amount (used to shift the ‘Shift_In’)
+  input [1:0] Mode; // To indicate SLL(00),  SRA(01), or ROR(10)
+  output [15:0] Shift_Out; //Shifter value
+
+  wire SLL, SRA, ROR;
+  assign SLL = (Mode == 2'b00);
+  assign SRA = (Mode == 2'b01);
+  assign ROR = (Mode == 2'b1x);
+
+  wire [15:0] shift0, shift1, shift2, shift3;
+
+  /*
+  if (Shift_Val[0]) begin
+    if (SLL) shift0 = {Shift_In[14:0], 1'b0};
+    else if (SRA) shift0 = {Shift_In[15], Shift_In[15:1]};
+    else shift0 = {Shift_In[0], Shift_In[15:1]};
+  end
+  */
+  assign shift0 = Shift_Val[0] ? (SLL ? {Shift_In[14:0], 1'b0} :
+                                  SRA ? {Shift_In[15], Shift_In[15:1]} :
+                                        {Shift_In[0], Shift_In[15:1]});
+
+  assign shift1 = Shift_Val[1] ? (SLL ? {shift0[13:0], 2'h0} :
+                                  SRA ? {{2{shift0[15]}}, shift0[15:2]} :
+                                        {shift0[0:1], shift0[15:2]}); //Does this reverse the order
+
+  assign shift2 = Shift_Val[2] ? (SLL ? {shift1[11:0], 4'h0} :
+                                  SRA ? {{4{shift1[15]}}, shift1[15:4]} :
+                                        {shift1[0:3], shift1[15:4]});
+
+  assign shift3 = Shift_Val[3] ? (SLL ? {shift2[7:0], 8'h0} :
+                                  SRA ? {{8{shift0[15]}}, shift1[15:8]} :
+                                        {shift0[0:7], shift0[15:8]});
+
+
+  assign shift0 = Shift_Val[0] ? (!Mode ? {Shift_In[15], Shift_In[15:1]} : {Shift_In[14:0], 1'b0}) : Shift_In;
+
+
+
+
+  assign shift1 = Shift_Val[1] ? (!Mode ? {{2{shift0[15]}}, shift0[15:2]} : {shift0[13:0], 2'b0}) : shift0;
+  assign shift2 = Shift_Val[2] ? (!Mode ? {{4{shift0[15]}}, shift1[15:4]} : {shift1[11:0], 4'b0}) : shift1;
+  assign Shift_Out = Shift_Val[3] ? (!Mode ? {{8{shift0[15]}}, shift2[15:8]} : {shift2[7:0],  8'b0}) : shift2;
+
+endmodule
