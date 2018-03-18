9b593396ea310ea67bcf8532312eeceb2c242a05
Devin Lafford <lafford@tux-33.cae.wisc.edu>
2018-03-18 17:54:00 -0500

    Changed memory.v to AND the addr with 0xFFFE

--- cpu/MEM/memory.v
+++ cpu/MEM/memory.v
@@ -4,6 +4,10 @@ module memory(clk, rst, alu_out, RegData2, MemOp, MemWrite, mem_out);
 	input [15:0] alu_out, RegData2;
 	output [15:0] mem_out;
 
+	wire addrwire;
+	
+	assign addrwire = {alu_out[15:1],1'b0};
+
 
 	memory1c DMem(.data_out(mem_out), .data_in(RegData2), .addr(alu_out),
 		.enable(MemOp), .wr(MemWrite), .clk(clk), .rst(rst));
