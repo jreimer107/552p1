bd53d806dde2daa4aaa769aa9667ecdef30fa493
jreimer107 <jreimer107@gmail.com>
2018-04-11 21:04:04 -0500

    mem fix

--- cpu/Phases/memory.v
+++ cpu/Phases/memory.v
@@ -4,6 +4,7 @@ module memory(clk, rst, alu_out, RegData2, MemOp, MemWrite, mem_out);
 	input [15:0] alu_out, RegData2;
 	output [15:0] mem_out;
 
-	memory1c DMem(.data_out(mem_out), .data_in(RegData2), .addr(alu_out << 1),
-		.enable(MemOp), .wr(MemWrite), .clk(clk), .rst(rst));
+	memory1c DMem(.data_out(mem_out), .data_in(RegData2), 
+		.addr({alu_out[15:1], 1'b0}), .enable(MemOp), .wr(MemWrite), 
+		.clk(clk), .rst(rst));
 endmodule
