7e50a8af14bf975c2ca18d077ed30cec8b5c17bd
Devin Lafford <lafford@tux-79.cae.wisc.edu>
2018-04-27 15:35:48 -0500

    pretty sure it passes test 3 now

--- cpu/Control/flag_reg.v
+++ cpu/Control/flag_reg.v
@@ -1,8 +1,9 @@
-module flag_reg(clk, rst, opcode, alu_ovfl, alu_out, NVZ);
+module flag_reg(clk, rst, opcode, alu_ovfl, alu_out, dis, NVZ);
     input clk, rst;
     input [3:0] opcode;
     input alu_ovfl;
     input [15:0] alu_out;
+    input dis;
 	output [2:0] NVZ;
 
     localparam  ADD = 4'b0000;
@@ -14,7 +15,7 @@ module flag_reg(clk, rst, opcode, alu_ovfl, alu_out, NVZ);
 
     //Write Enables to FLAG register
     wire [2:0] WriteEn; //NVZ order
-    assign WriteEn = (opcode === ADD || opcode === SUB) ? 3'b111 :
+    assign WriteEn = dis ? 3'b000 : (opcode === ADD || opcode === SUB) ? 3'b111 :
 	    (opcode === XOR || opcode === SLL || opcode === SRA || opcode === ROR) ? 3'b001 : 
             3'b000;
 
@@ -23,5 +24,5 @@ module flag_reg(clk, rst, opcode, alu_ovfl, alu_out, NVZ);
     assign NVZ_in = {alu_out[15], alu_ovfl, ~|alu_out};
     dff negative(.q(NVZ[2]), .d(NVZ_in[2]), .wen(WriteEn[2]), .clk(clk), .rst(rst));
 	dff overflow(.q(NVZ[1]), .d(NVZ_in[1]), .wen(WriteEn[1]), .clk(clk), .rst(rst));
-	dff zero(.q(NVZ[0]), .d(NVZ_in[0]), .wen(WriteEn[0]), .clk(clk), .rst(rst));
+	dff zero(.q(NVZ[0]), 	 .d(NVZ_in[0]), .wen(WriteEn[0]), .clk(clk), .rst(rst));
 endmodule
--- cpu/Phases/execute.v
+++ cpu/Phases/execute.v
@@ -26,6 +26,10 @@ module execute(clk, rst, instr, RegData1, RegData2, pcs, alu_out, LdByte, MemOp,
 	wire [15:0] ALUA, ALUB;
 	wire [6:0] ALUop;
 
+	wire dis;
+	
+	assign dis = (instr[11:8] == 4'b0000) ? 1'b1 : 1'b0;
+
 	ALU_Control ACTL(.instr(instr), .RegData1(RegData1), .RegData2(RegData2),
 		.pcs(pcs), .LdByte(LdByte), .MemOp(MemOp), .alu_out_MEM(alu_out_MEM),
 		.WriteData(WriteData), .ForwardA(ForwardA), .ForwardB(ForwardB),
@@ -35,6 +39,6 @@ module execute(clk, rst, instr, RegData1, RegData2, pcs, alu_out, LdByte, MemOp,
 		.ovfl(alu_ovfl));
 
 	flag_reg FLAG(.clk(clk), .rst(rst), .opcode(instr[15:12]),
-		.alu_ovfl(alu_ovfl), .alu_out(alu_out), .NVZ(NVZ));
+		.alu_ovfl(alu_ovfl), .alu_out(alu_out), .dis(dis), .NVZ(NVZ));
 
 endmodule
